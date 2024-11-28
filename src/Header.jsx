bac8bae3807b0fb8986725f0a69a9ae2ae4697cc
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2024-11-28 07:15:10 +0200

    add ant design and start

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div className="flex justify-end p-4 shadow-md">
+            All advantages
+        </div>
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      color: {
+        bg: '#f9fcff',
+        linear_btn1: '#10b1f0',
+        linear_btn2: '#6e81f8',
+        darker: '#f5f7f9',
+        order_review: '#0d0efe',
+        discount: '#83d557',
+        total: '#4f5fee'
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
